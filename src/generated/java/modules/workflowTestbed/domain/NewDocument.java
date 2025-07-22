17c1a96848a605981d55cdad20053180d1207494
Mat Forest <mat.forest@bizhub.com.au>
2025-07-22 11:29:47 +0930

    Initial foundry generation

---
+++ src/generated/java/modules/workflowTestbed/domain/NewDocument.java
@@ -0,0 +1,99 @@
+package modules.workflowTestbed.domain;
+
+import jakarta.annotation.Generated;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlTransient;
+import jakarta.xml.bind.annotation.XmlType;
+import org.skyve.CORE;
+import org.skyve.domain.messages.DomainException;
+import org.skyve.impl.domain.AbstractPersistentBean;
+
+/**
+ * New Document
+ * 
+ * @stereotype "persistent"
+ */
+@XmlType
+@XmlRootElement
+@Generated(value = "org.skyve.impl.generate.OverridableDomainGenerator")
+public class NewDocument extends AbstractPersistentBean {
+	/**
+	 * For Serialization
+	 * @hidden
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/** @hidden */
+	public static final String MODULE_NAME = "workflowTestbed";
+
+	/** @hidden */
+	public static final String DOCUMENT_NAME = "NewDocument";
+
+	/** @hidden */
+	public static final String descriptionPropertyName = "description";
+
+	/**
+	 * Description
+	 **/
+	private String description;
+
+	@Override
+	@XmlTransient
+	public String getBizModule() {
+		return NewDocument.MODULE_NAME;
+	}
+
+	@Override
+	@XmlTransient
+	public String getBizDocument() {
+		return NewDocument.DOCUMENT_NAME;
+	}
+
+	public static NewDocument newInstance() {
+		try {
+			return CORE.getUser().getCustomer().getModule(MODULE_NAME).getDocument(CORE.getUser().getCustomer(), DOCUMENT_NAME).newInstance(CORE.getUser());
+		}
+		catch (RuntimeException e) {
+			throw e;
+		}
+		catch (Exception e) {
+			throw new DomainException(e);
+		}
+	}
+
+	@Override
+	@XmlTransient
+	public String getBizKey() {
+		try {
+			return org.skyve.util.Binder.formatMessage("New Document", this);
+		}
+		catch (@SuppressWarnings("unused") Exception e) {
+			return "Unknown";
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return ((o instanceof NewDocument) && 
+					this.getBizId().equals(((NewDocument) o).getBizId()));
+	}
+
+	/**
+	 * {@link #description} accessor.
+	 * @return	The value.
+	 **/
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * {@link #description} mutator.
+	 * @param description	The new value.
+	 **/
+	@XmlElement
+	public void setDescription(String description) {
+		preset(descriptionPropertyName, description);
+		this.description = description;
+	}
+}
